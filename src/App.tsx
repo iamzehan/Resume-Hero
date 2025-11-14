9eac5c9afe7e852c3491e12a9d00863bdbede947
Zehan <zehan.kareem@gmail.com>
2025-11-15 02:57:31 +0600

    init(iamzehan): Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='
+    flex flex-col items-center 
+    w-full h-screen'>
+      <p className='font-bold text-2xl'>Welcome to CV Maker</p>
+      <div className='content flex'>
+        {/* layout Controller(Preview/Edit) */}
+        {/* Form */}
+        {/* View */}
+      </div>
+    </main>
+  )
+}
+
+export default App
