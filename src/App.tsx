13b1edd38722a977642361f9a22bad984175f37b
Zehan <zehan.kareem@gmail.com>
2025-11-15 12:06:50 +0600

    feat!(iamzehan): Added All components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import './App.css'
-
+import "./App.css";
+import Form from "./components/Form";
+import { useState } from "react";
+import {type Data} from './lib/utils'
 function App() {
-
+  const [data, setData] = useState<Data>({
+    name: "",
+    phone: "",
+    email: "",
+    education: [],
+    job: [],
+  });
+  function handleDataState(newData: Data) {
+    setData(newData);
+  }
   return (
-    <main className='
-    flex flex-col items-center 
-    w-full h-screen'>
-      <p className='font-bold text-2xl'>Welcome to CV Maker</p>
-      <div className='content flex'>
+    <main
+      className="
+    flex flex-col items-center
+    w-full h-screen"
+    >
+      <p className="font-bold text-2xl">Welcome to CV Maker</p>
+      <div className="content flex md:justify-center">
         {/* layout Controller(Preview/Edit) */}
-        {/* Form */}
+        <Form data={data} setData={handleDataState} />
         {/* View */}
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
