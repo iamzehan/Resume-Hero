a1586e00be9ad8e7b7b9c110bb0e2d9f2cbf63c0
Zehan <zehan.kareem@gmail.com>
2025-11-16 15:50:31 +0600

    update(iamzehan): Minor Style changes

--- src/components/Form/personal.tsx
+++ src/components/Form/personal.tsx
@@ -9,7 +9,7 @@ export default function Personal({
   setData: (data: Data) => void;
 }) {
   const formRef = useRef<HTMLFormElement>(null);
-  function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const rawData = Object.fromEntries(formData.entries());
@@ -17,7 +17,7 @@ export default function Personal({
       rawData.name as string,
       rawData.phone as string,
       rawData.email as string
-    )
+    );
     setData(data);
   }
 
@@ -29,16 +29,33 @@ export default function Personal({
         </legend>
         <label htmlFor="name">Full name </label>
         <input type="text" name="name" placeholder="Full name" required></input>
-        <label htmlFor="phone">Phone number </label>
-        <input type="text" name="phone" placeholder="Phone number" required></input>
-        <label htmlFor="email">Email </label>
-        <input type="email" name="email" placeholder="you@example.com" required></input>
+        <div className="flex flex-col gap-2 md:flex-row">
+          <div className="flex flex-col flex-1">
+            <label htmlFor="phone">Phone number </label>
+            <input
+              type="text"
+              name="phone"
+              placeholder="Phone number"
+              required
+            ></input>
+          </div>
+          <div className="flex flex-col flex-1">
+            <label htmlFor="email">Email </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="you@example.com"
+            ></input>
+          </div>
+        </div>
+
         <div className="self-end md:self-center py-10">
-          <button 
-          onClick={()=>formRef.current?.requestSubmit()}
-          className="btn-primary p-3 rounded-full md:rounded flex items-center gap-2">
+          <button
+            onClick={() => formRef.current?.requestSubmit()}
+            className="btn-primary p-3 md:px-3 md:py-1 rounded-full md:rounded flex items-center gap-2"
+          >
             <span className="hidden md:block text-xl">Add</span>
-            <Add fontSize="medium"/>
+            <Add fontSize="medium" />
           </button>
         </div>
       </fieldset>
