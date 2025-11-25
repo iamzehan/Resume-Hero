6896f8e810a47c276920aeb16080a73df63eeddc
Zehan <zehan.kareem@gmail.com>
2025-11-25 17:45:18 +0600

    update(iamzehan): Added source code link

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,6 +2,8 @@ import Educations from "./Education";
 import Personal from "./personal";
 import Jobs from "./Jobs";
 import { Data } from "../../lib/utils";
+import { GitHub } from "@mui/icons-material";
+
 export default function Form({
   data,
   setData,
@@ -13,6 +15,15 @@ export default function Form({
 }) {
   return (
     <div className="flex flex-col justify-center items-center">
+        <a 
+        className="
+        flex gap-2 items-center border p-2 
+        rounded shadow text-black bg-white w-fit font-medium
+        xl:fixed md:top-20 xl:right-0 transform-all duration-300 xl:translate-x-[70%] xl:hover:translate-x-0
+        xl:animate-bounce hover:animate-none
+        "
+        href="https://github.com/iamzehan/Resume-Hero" rel="noopener" target="_blank">
+        <GitHub fontSize="small" className="text-purple-500"/> Source </a>
         <Personal data={data} setData={setData}/>
         <Educations data={data} setData={setData} setNotification={setNotification}/>
         <Jobs data={data} setData={setData} setNotification={setNotification}/>
